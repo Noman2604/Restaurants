e358082aaafb6547d0efb26de4d2af3b94f287af
noman2604 <nomanpatel07059@gmail.com>
2024-10-01 22:57:41 +0530

    Fixed next.config.mjs and added export script

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-export default {
-    output: 'export',
-    basePath: '/Restaurants',
-    assetPrefix: '/Restaurants/',
-    trailingSlash: true,
-};
 
+export default nextConfig;
