ac6e65ff403c70c0726ea26b04b1e40c4e1aeef9
noman2604 <nomanpatel07059@gmail.com>
2024-10-01 22:35:16 +0530

    Fixed next.config.mjs and added export script

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+module.exports = {
+    output: 'export',
+    basePath: '/Restaurants', // replace with your GitHub repo name
+    assetPrefix: '/Restaurants/', // replace with your GitHub repo name
+    trailingSlash: true, // ensure trailing slashes for all URLs
+};
 
-export default nextConfig;
+ 
+module.exports = nextConfig ;
